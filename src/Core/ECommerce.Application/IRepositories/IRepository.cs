eb7b75d8b5bcc80b648c6134aa4e7a14ea02189a
Enes Ozmus <ensozmus@gmail.com>
2022-04-30 19:42:31 +0300

    #Lesson 4 | Onion Architecture and Repository Pattern

---
+++ src/Core/ECommerce.Application/IRepositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using ECommerce.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Application.IRepositories
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        DbSet<T> Table { get; }
+    }
+}
