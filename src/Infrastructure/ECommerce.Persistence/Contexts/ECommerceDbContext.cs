b22f377472a9bc8a6e37fab3186dc5bf6f217662
Enes Ozmus <ensozmus@gmail.com>
2022-04-30 20:36:40 +0300

    #Lesson 5 | Contexts and Generic Repository Design Pattern

---
+++ src/Infrastructure/ECommerce.Persistence/Contexts/ECommerceDbContext.cs
@@ -0,0 +1,38 @@
+﻿using ECommerce.Domain.Entities;
+using ECommerce.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Persistence.Contexts
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public ECommerceDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        #region SaveChangesAsync
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            //ChangeTracker : Entityler üzerinden yapılan değişiklerin ya da yeni eklenen verinin yakalanmasını sağlayan propertydir.
+            //Update operasyonlarında Track edilen verileri yakalayıp elde etmemizi sağlar.
+
+            var datas = ChangeTracker
+                 .Entries<BaseEntity>();
+
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.Now,
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.Now,
+                    _ => DateTime.Now
+                };
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+        #endregion
+    }
+}
